a469a239f9df1f089b467574b071af10cd7ad4f6
Wonjae Kim <wonjaek36@gmail.com>
2022-02-19 19:44:57 +0900

    [Baekjoon] Python 11279

---
+++ baekjoon/11279/11279.py
@@ -0,0 +1,76 @@
+import sys
+from sys import stdin
+
+
+class MaxHeap(object):
+
+    def __init__(self):
+        self._heap = [-1]*100002
+        self._size = 0
+
+    def hpush(self, v):
+        self._size += 1
+
+        pos = self._size
+        while pos > 1:
+            if self._heap[pos//2] < v:
+                self._heap[pos] = self._heap[pos//2]
+                pos //= 2
+            else:
+                break
+
+        self._heap[pos] = v
+
+    def hpop(self):
+
+        if self._size == 0:
+            return 0
+        
+        v = self._heap[1]
+        
+        pos = 1
+        self._size -= 1
+        while pos <= self._size:
+            left = pos*2
+            right = pos*2+1
+
+            if (left <= self._size and right <= self._size):
+                if self._heap[left] > self._heap[right] and\
+                   self._heap[left] > self._heap[self._size+1]:
+                    self._heap[pos] = self._heap[left]
+                    pos = left
+
+                elif self._heap[left] <= self._heap[right] and\
+                   self._heap[right] > self._heap[self._size+1]:
+                    self._heap[pos] = self._heap[right]
+                    pos = right
+
+                else:
+                    break
+
+            elif left <= self._size:
+                if self._heap[left] > self._heap[self._size+1]:
+                    self._heap[pos] = self._heap[left]
+                    pos = left
+                else:
+                    break
+
+            else:
+                break
+
+        self._heap[pos] = self._heap[self._size+1]
+        self._heap[self._size+1] = -1
+
+        return v
+    
+
+if __name__ == '__main__':
+
+    h = MaxHeap()
+    T = int(stdin.readline())
+    for i in range(T):
+        v = int(stdin.readline())
+        if v == 0:
+            print(h.hpop())
+        else:
+            h.hpush(v)
