5db1c3379be8bfd016dfbe97c941ea9cfb9c8177
Wonjae Kim <wonjaek36@gmail.com>
2022-03-31 00:46:11 +0900

    [Baekjoon] Python 1167

---
+++ baekjoon/1167/1167.py
@@ -0,0 +1,13 @@
+from sys import stdin
+
+
+class TreeDiameter(object):
+
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    n = int(stdin.readline())
+    for _ in range(n):
+        stdin.readline().rstrip().split(' ')
